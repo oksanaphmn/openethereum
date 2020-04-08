be42a03874e4bbb8e2cd525e5c76c750bf3a9b92
leviathan88 <elvisdedic@outlook.com>
2020-04-08 08:28:21 +0200

    added extra check for our incorrectly marked bad blocks

--- ethcore/verification/src/queue/kind.rs
+++ ethcore/verification/src/queue/kind.rs
@@ -84,11 +84,17 @@ pub mod blocks {
 		block::PreverifiedBlock,
 		errors::{EthcoreError as Error, BlockError},
 		verification::Unverified,
+		BlockNumber,
 	};
 	use log::{debug, warn};
 	use crate::verification::{verify_block_basic, verify_block_unordered};
 
 	use ethereum_types::{H256, U256};
+
+	// ! qiibee specific code
+	// ! IDs of first and last bad block
+	const INITIAL_BAD_BLOCK: BlockNumber = 340_891;
+	const LAST_BAD_BLOCK: BlockNumber = 513_381;
 
 	/// A mode for verifying blocks.
 	pub struct Blocks;
@@ -111,7 +117,15 @@ pub mod blocks {
 				},
 				Err(e) => {
 					warn!(target: "client", "Stage 1 block verification failed for {}: {:?}", input.hash(), e);
-					Err((e, Some(input)))
+					// ! qiibee specific code
+					// ? if error occurrs within this range, inside of this verification only
+					// ? we send Ok() since those blocks are actually fine					
+					if input.header.number() >= INITIAL_BAD_BLOCK && input.header.number() <= LAST_BAD_BLOCK {
+						Ok(input)
+					} else {
+						warn!("it failed for ${}", input.header.number());
+						Err((e, Some(input)))
+					}					
 				}
 			}
 		}
